1028891142de9cbf694848b50577de47070bf44c
gowthamsunnam <gowthams123g@gmail.com>
2026-04-02 20:56:27 +0530

    Removed frontend from repo (kept locally)

--- TextMorphAI/frontend/readability.py
+++
@@ -1,8 +0,0 @@
-import textstat
-
-def get_readability(text):
-    return {
-        "Flesch Reading Ease": textstat.flesch_reading_ease(text),
-        "Gunning Fog": textstat.gunning_fog(text),
-        "SMOG Index": textstat.smog_index(text)
-    }
